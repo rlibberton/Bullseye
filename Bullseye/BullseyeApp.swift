365ecaefc3b75506208d8f3c2b4f1ae58a5a3b60
rlibbert <rlibbert@uncc.edu>
2022-02-03 10:13:36 -0500

    Initial Commit

---
+++ Bullseye/BullseyeApp.swift
@@ -0,0 +1,17 @@
+//
+//  BullseyeApp.swift
+//  Bullseye
+//
+//  Created by Racheal Libberton on 2/3/22.
+//
+
+import SwiftUI
+
+@main
+struct BullseyeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Bullseye/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Bullseye
+//
+//  Created by Racheal Libberton on 2/3/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
