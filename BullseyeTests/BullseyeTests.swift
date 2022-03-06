30a1b77893eb6938b4c4c28ed16e77e9aaa70266
rlibbert <rlibberton@gmail.com>
2022-03-06 17:29:58 -0500

    Added first unit test

---
+++ BullseyeTests/BullseyeTests.swift
@@ -0,0 +1,27 @@
+//
+//  BullseyeTests.swift
+//  BullseyeTests
+//
+//  Created by Racheal Libberton on 3/6/22.
+//
+
+import XCTest
+@testable import Bullseye
+
+class BullseyeTests: XCTestCase {
+    
+    var game: Game!
+
+    override func setUpWithError() throws {
+        game = Game()
+    }
+
+    override func tearDownWithError() throws {
+        game = nil
+    }
+
+    func testExample() throws {
+        XCTAssertEqual(game.points(sliderValue: 50), 999)
+    }
+
+}
